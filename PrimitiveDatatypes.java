f66c2dd0ac7360ee76b77b24829711ed4556937e
raulriverarojas <72164424+raulriverarojas@users.noreply.github.com>
2022-12-28 17:51:38 -0500

    Update PrimitiveDatatypes.java

--- PrimitiveDatatypes.java
+++ PrimitiveDatatypes.java
@@ -1,5 +1,5 @@
 public class PrimitiveDatatypes {
-  public static void main(String args[]){
+  public static void main(String[] args){
     int num=58;
     System.out.println(num);
     System.out.println("yeah");
